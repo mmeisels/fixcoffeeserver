2a68a8ddf270fc60b84f1a5a01ebf9a7fc146abd
Mike Meisels <mmeisels@salesforce.com>
2018-07-24 13:30:03 +1000

    Changed User login in auth.js

--- server/auth.js
+++ server/auth.js
@@ -197,8 +197,6 @@ function createUser(user, password) {
                 var pictureURL = '';
                 winston.info('DB Query');
                 db.query('INSERT INTO salesforce.contact (email, password__c, firstname, lastname, leadsource, loyaltyid__c, accountid) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id, firstName, lastName, email, gender__c as gender, pictureURL__c as pictureURL, loyaltyid__c as externalUserId'
-                    //RETURNING id, firstName, lastName, email, gender__c as gender, pictureURL__c as pictureURL, loyaltyid__c as externalUserId'
-                    //RETURNING id, firstName, lastName, email, contact__loyaltyid__c as externalUserId',
                    [user.email, password, user.firstName, user.lastName, 'Loyalty App', externalUserId, config.contactsAccountId], true)
                 .then(function (insertedUser) {
                     winston.info('Inserted Query');
